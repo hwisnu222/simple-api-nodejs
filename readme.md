97f4c358d7684cbbe2f96fc30fb9f26d6b88b0ca
WisnuHarjanta <hwisnu222@gmail.com>
2020-12-11 20:34:20 +0700

    add readme.md

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Simple Api Node js
+
+## installation
+
+Install package
+
+```
+npm install
+```
+
+Run your project
+
+```
+node server
+```
+
+Test project using postman or your browser
+happy day
--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@
 module.exports = function (app) {
   const myjson = require("./controller");
 
+  // routes
   app.route("/").get(myjson.index);
   app.route("/view").get(myjson.viewAllData);
   app.route("/view/:id").get(myjson.viewStudentById);

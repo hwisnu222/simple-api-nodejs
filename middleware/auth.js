2879cc1b3becb2da3a06cea8cce2c1ac44dd50dc
hwisnu222 <hwisnu222@gmail.com>
2020-12-23 21:14:55 +0700

    add routes for registration in middleware directory

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,4 +15,30 @@ exports.registrasi = function (req, res) {
     role: req.body.role,
     tanggal_daftar: new Date(),
   };
+
+  let query = "SELECT email FROM ?? WHERE ??=?";
+  const table = ["user", "email", post.email];
+
+  query = mysql.format(query, table);
+  connection.query(query, table, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 0) {
+        let query = "INSERT INTO ?? SET ?";
+        const table = ["user"];
+
+        query = mysql.format(query, table);
+        connection.query(query, post, function (error, rows) {
+          if (error) {
+            console.log(error);
+          } else {
+            response.ok("Berhasil menambahkan data user baru", res);
+          }
+        });
+      } else {
+        response.ok("email sudah terdaftar", res);
+      }
+    }
+  });
 };
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 // parse application/json data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 // call routes
 const routes = require("./routes");
 routes(app);
+
+// register auth menu from index
+app.use("/auth", require("./middleware"));
 
 // create server
 app.listen(3000, () => {
